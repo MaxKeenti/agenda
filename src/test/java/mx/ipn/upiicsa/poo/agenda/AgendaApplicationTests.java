48b4f0696d8e50a1e8e2ed1656728c38f2914bff
Max Gonzalez <52800035+MaxKeenti@users.noreply.github.com>
2024-12-03 10:16:28 -0600

    First commit

---
+++ src/test/java/mx/ipn/upiicsa/poo/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.ipn.upiicsa.poo.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
